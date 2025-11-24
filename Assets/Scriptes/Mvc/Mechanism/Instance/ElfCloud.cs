299160678276ba6cb197bc3476d12cdeec0a85d6
dengweihao <dengweihao@playorcas.com>
2025-11-24 18:17:09 +0800

    llll

--- Assets/Scriptes/Mvc/Mechanism/Instance/ElfCloud.cs
+++ Assets/Scriptes/Mvc/Mechanism/Instance/ElfCloud.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ElfCloud :Mechanism
 {
+    public int GetGroup() {
+        return Model.IntData;
+    }
+
     protected override void ShowInEditor()
     {
         base.ShowInEditor();
+
+        transform.Find("GroupPanel").GetComponent<TextMeshProUGUI>().text = GetGroup().ToString();
     }
 }
