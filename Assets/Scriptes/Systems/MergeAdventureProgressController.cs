1aa5a7cf5f4268ca485ccec4aa83102c28c39d5f
dengweihao <dengweihao@playorcas.com>
2026-01-26 15:55:43 +0800

    阿三大苏打

--- Assets/Scriptes/Systems/MergeAdventureProgressController.cs
+++ Assets/Scriptes/Systems/MergeAdventureProgressController.cs
@@ -14,7 +14,7 @@ public class MergeAdventureProgressController : GameProgressController
 
     public static MergeAdventureProgressController _instance;
 
-    public const int _testLevel = 102;
+    public const int _testLevel = 110;
 
     protected override void InitProgress()
     {
